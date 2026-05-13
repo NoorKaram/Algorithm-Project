a88703205ec2619c745f45cadd5462e81b710aa8
Nourhan sabry <nourhansabry611@gmail.com>
2026-05-13 18:45:39 +0300

    Update MaxSubarrayRecursive.java

--- MaxSubarrayRecursive.java
+++ MaxSubarrayRecursive.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class MaxSubarrayRecursive {
 
     public static int maxSubArray(int[] nums) {
+
         int[] maxSum = {nums[0]};
+
         helper(nums, nums.length - 1, maxSum);
+
         return maxSum[0];
     }
 
     private static int helper(int[] nums, int index, int[] maxSum) {
+
         if (index == 0) {
             return nums[0];
         }
@@ -16,6 +20,7 @@ public class MaxSubarrayRecursive {
         int prev = helper(nums, index - 1, maxSum);
 
         int current;
+
         if (prev + nums[index] > nums[index]) {
             current = prev + nums[index];
         } else {
@@ -30,18 +35,56 @@ public class MaxSubarrayRecursive {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int[] nums = new int[n];
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
+        String input;
+
+        int index;
+
+        while (true) {
+
+            System.out.print("Enter number of elements: ");
+
+            input = scanner.next();
+
+            boolean valid = true;
+
+            for (int i = 0; i < input.length(); i++) {
+
+                char c = input.charAt(i);
+
+                if (c < '0' || c > '9') {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid) {
+
+                index = Integer.parseInt(input);
+
+                break;
+
+            } else {
+
+                System.out.println("Please enter a valid number:");
+            }
+        }
+
+        int[] nums = new int[index];
+
+        System.out.println("Enter the elements:");
+
+        for (int i = 0; i < index; i++) {
+
+            nums[i] = scanner.nextInt();
         }
 
         int result = maxSubArray(nums);
-        System.out.println(result);
 
-        sc.close();
+        System.out.println("Maximum Subarray Sum = " + result);
+
+        scanner.close();
     }
 }
